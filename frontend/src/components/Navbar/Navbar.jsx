9778cd01f120a4d90d509c47726d6de328b91a8e
RAJ kr Rabidas <das752101@gmail.com>
2025-08-05 00:06:04 +0530

    create navbar

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <header>
+        <h2>Logo</h2>
+      <nav>
+        <ul>
+          <li><a href=""></a>Home</li>
+          <li><a href=""></a>About</li>
+          <li><a href=""></a>Post</li>
+          <li><a href=""></a>Sing in</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
